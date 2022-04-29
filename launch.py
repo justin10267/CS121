b132d17e92f645264a10cb9657dcb741933e157e
justin10267 <93513655+justin10267@users.noreply.github.com>
2022-04-28 23:08:34 -0700

    Update launch.py

--- launch.py
+++ launch.py
@@ -4,7 +4,7 @@ from argparse import ArgumentParser
 from utils.server_registration import get_cache_server
 from utils.config import Config
 from crawler import Crawler
-
+from scraper import fragmented_urls, most_common_words, longest_page, ics_uci_edu_subdomains
 
 def main(config_file, restart):
     cparser = ConfigParser()
@@ -21,3 +21,14 @@ if __name__ == "__main__":
     parser.add_argument("--config_file", type=str, default="config.ini")
     args = parser.parse_args()
     main(args.config_file, args.restart)
+    print("Unique Pages: {}".format(len(fragmented_urls)))
+    print("Longest Page (based on words): {}".format(longest_page))
+    sorted_common_words_list = [(word, freq) for word, freq in sorted(most_common_words.items(), key=lambda item: item[1], reverse=True)]
+    top_50_common_words = sorted_common_words_list[:50]
+    print("TOP 50 COMMON WORDS")
+    for word in top_50_common_words:
+        print("Word: {}. Freq: {}.".format(word[0], word[1]))
+    sorted_ics_uci_edu = [(domain, unique_pages) for domain, unique_pages in sorted(ics_uci_edu_subdomains.items(), key=lambda item: item[0])]
+    print("ICS UCI EDU SUBDOMAINS")
+    for domain in sorted_ics_uci_edu:
+        print("{}, {}".format(domain[0], domain[1]))
